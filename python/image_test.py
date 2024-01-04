6229cbb760c4b48f6a0d8f7c1ada28eea283b70c
wisekim <lwisekiml@naver.com>
2024-01-04 23:09:52 +0900

    보조장비 딕셔너리를 만들어 어떤 보조장비를 가지고 있는지 확인할 수 있게함

--- python/image_test.py
+++ python/image_test.py
@@ -1,14 +1,15 @@
-# 참고 : https://re-code-cord.tistory.com/entry/OpenCV-Template-Matching-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
 import cv2
 import numpy as np
 import os
 
-img_rgb = cv2.imread('test.jpg')
+ae = {"athena" : 0, "bul" : 0, "ekey" : 0, "go" : 0, "hera" : 0, "jiben" : 0, "ni" : 0, "time" : 0}
+
+img_rgb = cv2.imread('test1.jpg')
+
 
 for (root, directories, files) in os.walk("data/Auxiliary_equipment_new"):
     for file in files:
         file_path = os.path.join(root, file)
-        print("file_path : ", file_path)
 
         template = cv2.imread(file_path)
         h, w = template.shape[:-1]
@@ -17,10 +18,15 @@ for (root, directories, files) in os.walk("data/Auxiliary_equipment_new"):
         threshold = .76
         loc = np.where(res >= threshold)
 
-        i = 0
         for pt in zip(*loc[::-1]):  # Switch collumns and rows
-            i = i + 1
+            # 문자열 파싱        
+            file_name = file_path.split('\\')[1].split('.')[0]
+            print("file_name : ", file_name)
+            ae[file_name] += 1
+            #######
             cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 1)
+
+print("ae : ", ae)
 
 cv2.imshow("img_rgb", img_rgb)
 cv2.waitKey(0)
