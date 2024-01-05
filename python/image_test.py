9905228e8120a95c44283bfffb4d53ad3eb5d89c
wisekim <lwisekiml@naver.com>
2024-01-05 22:40:18 +0900

    보조장비, 악세사리 이미지 찾기 적용 및 어떤 장비가 있는지 확인 가능하게 수정

--- python/image_test.py
+++ python/image_test.py
@@ -2,14 +2,23 @@ import cv2
 import numpy as np
 import os
 
-ae = {"athena" : 0, "bul" : 0, "ekey" : 0, "go" : 0, "hera" : 0, "jiben" : 0, "ni" : 0, "time" : 0}
+auxiliary_equipment = {"ekey":0, "hera":0, "jiben":0, "bul":0, "athena":0, "time":0, "ni":0, "go":0}
+accessories = {"am":0, "pi":0, "zip":0, "botiban":0, "botipal":0, "botimok":0, "geoban":0, "geopal":0, "geomok":0, "jungban":0, "jungpal":0, "jungmok":0, "superring":0, "superarm":0, "supermok":0}
+armor = {}
 
-img_rgb = cv2.imread('test1.jpg')
+# 분석할 이미지
+img_rgb = cv2.imread('asd.jpg')
 
+for (root, directories, files) in os.walk("data"):
 
-for (root, directories, files) in os.walk("data/Auxiliary_equipment_new"):
+    # 폴더 전체 경로
+    for d in directories:
+        d_path = os.path.join(root, d)
+        print("d_path : ", d_path) # data\Auxiliary_equipment
+
     for file in files:
-        file_path = os.path.join(root, file)
+        file_path = os.path.join(root, file) # data\Accessories_new\am.jpg
+        print("file_path : ", file_path)
 
         template = cv2.imread(file_path)
         h, w = template.shape[:-1]
@@ -18,16 +27,28 @@ for (root, directories, files) in os.walk("data/Auxiliary_equipment_new"):
         threshold = .76
         loc = np.where(res >= threshold)
 
+        # 스크린샷에 없는 이미지일 경우 for에 안들어 온다.
         for pt in zip(*loc[::-1]):  # Switch collumns and rows
-            # 문자열 파싱        
-            file_name = file_path.split('\\')[1].split('.')[0]
+            # 문자열 파싱
+            directory_name = file_path.split('\\')[1].split('.')[0]
+            file_name = file_path.split('\\')[2].split('.')[0]
+            print("directory_name : ", directory_name)
             print("file_name : ", file_name)
-            ae[file_name] += 1
+
+            if directory_name == 'Accessories':
+                accessories[file_name] += 1
+            elif directory_name == 'Auxiliary_equipment':
+                auxiliary_equipment[file_name] += 1
+            elif directory_name == 'Armor':
+                armor[file_name] += 1
             #######
             cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 1)
 
-print("ae : ", ae)
+print("auxiliary_equipment : ", auxiliary_equipment)
+print("accessories : ", accessories)
 
-cv2.imshow("img_rgb", img_rgb)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite('result.jpg', img_rgb)
+
+# cv2.imshow("img_rgb", img_rgb)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
